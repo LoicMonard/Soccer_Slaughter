cba2066237474aa8e02fee19f4e6d68ce6cf9013
LoicMonard <bobbycarotte@outlook.fr>
2018-06-30 00:57:54 +0200

    UserAgent introduced

---
+++ Soccer_Slaughter/Assets/UserAgentEngine.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserAgentEngine : MonoBehaviour {
+
+    public Transform _target;
+    public float _speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(_target);
+        transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+	}
+}
